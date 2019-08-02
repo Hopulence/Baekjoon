933a53b392217ecacdf161a25ed191caf3e5bcab
Hopulence <41990924+Hopulence@users.noreply.github.com>
2019-08-02 16:04:06 +0900

    Create 2775 부녀회장.c

---
+++ 2775 부녀회장.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+	int test_case;
+	int k,n;
+	int i=0, j;
+	
+	scanf("%d", &test_case);
+	int n[test_case];
+	
+	while(i<test_case) {
+		
+		scanf("%d", &k);
+		scanf("%d", &n);
+		
+		if(n==1)
+			n[i] == 1;
+		
+		j=0;
+		else {
+			while(j<k) {
+				n[i] = n*(n+1)/2
+			}
+		}
+		
+		i++;
+	}
+	
+	for(i=0;i<test_case;i++)	
+		printf("%d\n", n[i]);
+
+	return 0;
+}
